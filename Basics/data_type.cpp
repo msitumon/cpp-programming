3c8021716de0834e337207c9b814f9d6d5579d42
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-02 19:14:24 +0600

    In C++, variables are containers for storing data values

---
+++ Basics/data_type.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Numerical data types
+    int myRegistrationNum=27;
+    float myheight=5.64;
+    double myWaight=61.1055;
+    char grade='A';
+    string myName="Jaodun";
+
+    /*Output of the previous variable
+    Using cout function*/
+    cout  <<"Registration number: " <<myRegistrationNum <<"\n";
+    cout << "My height: " <<myheight <<"\n";
+    cout << "My waight is: " <<myWaight <<"\n";
+    cout << "Grade " <<grade <<"\n";
+    cout << "MY name is: " <<myName <<"\n";
+return 0;
+}
