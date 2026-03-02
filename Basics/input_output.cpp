452b934895f09fad1608433e41b29efda47d1f12
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-02 06:07:57 +0600

    Basic input/output program using cin and cout

---
+++ Basics/input_output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Simple input output program
+    int x,y,sum;
+    cout << "Enter the value of x: ";
+    cin >>x;
+    cout << "Enter the value of y: ";
+    cin >>y;
+    sum = x + y;
+    cout << "Summation of x and y is: " << sum;
+return 0;
+}
