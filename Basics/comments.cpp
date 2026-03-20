2e1ac5ec0ed0aa514890047808db3cb793445991
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-21 01:58:19 +0600

    Comment can be single line or multi lined.

---
+++ Basics/comments.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //This is a single line comment
+    cout << "Single line comment start with two forward slashes.\n";
+    /*This code below will printf rules of creating a comment.
+    Comments can be used to explain C++ code and
+    to make it more readable.*/
+    cout <<"Multi line comment start with one forward slashe and one staric. End with one staric and one forward slashe";
+    return 0;
+}
