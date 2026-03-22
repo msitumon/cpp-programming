24f0ecd9073ea80454299a8d76a0c78a55f86743
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-22 16:21:56 +0600

    cin is a  predefined variable that reads data from the keyboard.

---
+++ Basics/user_input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout <<"Enter your age: ";
+    cin >> age;
+    cout <<"Your are " << age <<" years old.\n";
+    float height;
+    cout <<"Enter your height: ";
+    cin >> height;
+    cout <<"Your height is " << height;
+return 0;
+}
