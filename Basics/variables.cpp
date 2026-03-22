9bcf553b44ece2e8b12e73ecbbb0ec850cfbe56c
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-22 16:09:24 +0600

    Variable are containers for storing data values.

---
+++ Basics/variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //type variableName = value;
+    int age=22, option=2;
+    float height=1.7;
+    //const type variableName = value;
+    const double pi=3.14;
+    char grate='A';
+    string name="Samiul Islam";
+    bool myBoolean=true;
+
+    cout << "My name is " << name << " and I am " << age <<" years old.";
+    cout <<"My height is " << height <<"\nMy optain grade is " << grate;
+    cout <<"\nThe constant value of pi is " << pi;
+
+return 0;
+}
