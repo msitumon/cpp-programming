a36ccd49f9b82f9e4a8391ce88206245ed29fcbf
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-23 23:39:17 +0600

    How auto can be used to create variables of different types, based on the values you assign.

---
+++ Basics/auto_keyword.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Creating auto variables
+    auto age=22;
+    auto height=1.7f;
+    auto gpa=3.33;
+    auto grade='B';
+    auto name="Samiul Islam Tumon. ";
+
+    //Print variable values
+    cout << "My name is " << name;
+    cout << "I am " << age <<" years old.\n";
+    cout << "My height is " << height << " meter.\n";
+    cout << "SGPA is " << gpa <<" and grade " << grade << "\n";
+return 0;
+}
