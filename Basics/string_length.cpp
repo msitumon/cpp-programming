333838754f28e69a493a99fc160a45348999ab22
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-26 23:57:55 +0600

    use the size() function to get the length of a string.

---
+++ Basics/string_length.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string text="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout <<"The length of the text string is: " <<text.size();
+return 0;
+}
