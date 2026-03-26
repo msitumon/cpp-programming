045cf9301de4c1104149a51f784d5e5bfead2590
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-26 21:04:22 +0600

    Use the getline() function to read a line of text.

---
+++ Basics/user_intput_string.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string fullName;
+    cout <<"Type your full name: ";
+    getline (cin, fullName);
+    cout <<"Your name is: " <<fullName;
+    return 0;
+}
