9eef230ab7d0e657c8c5e4dfba79e55d39c2ef01
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-28 01:38:26 +0600

    True value returns 1 and false returns 0.

---
+++ Basics/boolean_value.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun <<"\n";
+    cout << isFishTasty <<"\n";
+    cout << boolalpha;
+    cout << isCodingFun <<"\n";
+    cout << isFishTasty <<"\n"; 
+return 0;
+}
