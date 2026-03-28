2b214898675aaf567263308bcaf373b2b5433906
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-29 01:27:55 +0600

    Else statement to specify a block of code to be executed if th econdition is false.

---
+++ If ... Else/else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int time;
+    cout <<"Enter the time: ";
+    cin >> time;
+    if(time<18){
+        cout <<"Good day.";
+    }
+    else {
+        cout <<"Good evening.";
+    }
+return 0;
+}
